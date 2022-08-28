f30d7347e8a971950a0b16bcc2d037d9cbc0848d
Ali Fahed <alifahed67@gmail.com>
2022-08-28 21:19:53 +0800

    update commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const person = require("./person");
+// person.js file
+// const person = require("./person");
+// console.log(person.name);
 
-console.log(person.name);
+// logger.js file
+// const Logger = require("./logger");
+// const logger = new Logger();
+// logger.on("message", (data) => console.log("Called Listener:", data));
+// logger.log("Hello World");
---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require("os");
+
+// platform
+console.log(os.platform());
+
+// CPU Architecture
+console.log(os.arch());
+
+// CPU Core Info
+// console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Total memeory
+console.log(os.totalmem);
+
+// Home dir
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
